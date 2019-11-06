3f786b2a4bd83fc9dd07c8eb3ba87733152a9585
田羽衡 <958245365@qq.com>
2019-11-06 13:10:28 +0800

    基础提交

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,126 @@
 import 'package:flutter/material.dart';
 
+import 'main2.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Welcome to Flutter',
+
+      home: IndexPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('第二页'),
+        leading: FlutterLogo(colors: Colors.lightGreen),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: Padding(
+        padding: EdgeInsets.all(30.0),
+        child: RaisedButton(
+            child: Text('回到上一页'),
+            onPressed: () {
+              Navigator.pop(context); //2
+            }),
+      ),
+    );
+  }
+}
+
+
+class IndexPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    var ok =
+        "https://gss3.bdstatic.com/-Po3dSag_xI4khGkpoWK1HF6hhy/baike/w%3D268%3Bg%3D0/sign=9bc84be4289759ee4a5067cd8ac0242b/94cad1c8a786c917ec0c7dd0c73d70cf3ac757cf.jpg";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Basics Widget'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: () {
+              print('添加按钮');
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: Center(
+          child: Text('抽屉'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      body: Padding(
+          padding: EdgeInsets.all(0.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                '文本样式',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.indigo,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '文本样式',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.indigo,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Image.network(
+                ok,
+                width: 200,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  print("onPressed");
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => OkApp()));
+                  ok = "https://s2.ax1x.com/2019/05/27/VZrNNR.png";
+
+                },
+                color: Colors.red,
+                textColor: Colors.blue,
+                child: Image.network(
+                    "https://s2.ax1x.com/2019/05/27/VZrNNR.png"),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                     child: Text("将屏幕横向评分两份,我是第一份"),
+                      flex: 1,
+                    ),
+                    Expanded(
+                      child: Text("将屏幕横向评分两份,我是第二份"),
+                      flex: 2,
+                    ),
+                    Container(
+                      child: Column(),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )),
     );
   }
 }
